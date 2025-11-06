ef38c8afe40bfac42bf0d2e36b8e743f7d53ba03
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-06 22:48:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import LocationSelection from "./LocationSelection";
+// import LocationSelector from "./LocationSelector";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LocationSelector /> */}
+      <LocationSelection />
+      {/* <h1>Welcome to the My-App</h1> */}
+    </div>
+  );
+}
+
+export default App;
